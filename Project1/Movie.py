0cf532d02a4daec6174785a474b235faa0398ad7
pdragosh <pam.dragosh@gmail.com>
2015-06-02 15:50:55 -0400

    Update Movie.py
    use 4 spaces instead of tab - added comments.

--- Project1/Movie.py
+++ Project1/Movie.py
@@ -1,5 +1,6 @@
 import fresh_tomatoes
 
+# Definition of our Movie class
 class Movie:
     title= '';
     poster_image_url = '';
@@ -12,16 +13,18 @@ class Movie:
         self.trailer_youtube_url = youtube;
         self.imdb = imdb;
 
+# This is the array of movies
 movies = [];
 
 movies.append(Movie('Clueless',
-                    'http://upload.wikimedia.org/wikipedia/en/2/21/Clueless.jpg',
-                    'https://www.youtube.com/watch?v=RS0KyTZ3Ie4',
-                    'http://www.imdb.com/title/tt0112697/'));
+    'http://upload.wikimedia.org/wikipedia/en/2/21/Clueless.jpg',
+    'https://www.youtube.com/watch?v=RS0KyTZ3Ie4',
+    'http://www.imdb.com/title/tt0112697/'));
 
 movies.append(Movie('Uncle Buck',
-                    'http://upload.wikimedia.org/wikipedia/en/8/8c/Uncle_buck.jpg',
-                    'https://www.youtube.com/watch?v=zXEzA1egFL4',
-                    'http://www.imdb.com/title/tt0098554/?ref_=nv_sr_1'));
+    'http://upload.wikimedia.org/wikipedia/en/8/8c/Uncle_buck.jpg',
+    'https://www.youtube.com/watch?v=zXEzA1egFL4',
+    'http://www.imdb.com/title/tt0098554/?ref_=nv_sr_1'));
 
+# Call the code to open a browser and display our favorite movies
 fresh_tomatoes.open_movies_page(movies);
